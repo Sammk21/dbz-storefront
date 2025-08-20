bbecb5f015e97862e77ade19a881ec2210a9f672
sameer khan <05sameerk@gmail.com>
2025-08-21 01:13:53 +0530

    added analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import LenisProvider from "@modules/lenis"
 import { Metadata } from "next"
 import "styles/globals.css"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
@@ -15,6 +16,12 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         {/* <LenisProvider> */}
         <main className="relative">{props.children}</main>
         {/* </LenisProvider> */}
+        <Script
+          defer
+          src="https://logs.dividebzero.in/script.js"
+          data-website-id="74d17f51-3cf2-4dfc-91b5-567201bf1a88"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   )
